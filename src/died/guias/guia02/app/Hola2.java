0151fd07068d5a03fcd960853039d9bbc0c4a822
TomasRavelli <tomy_ravelli@hotmail.com>
2018-04-10 11:26:02 -0300

    version inicial

---
+++ src/died/guias/guia02/app/Hola2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package died.guias.guia02.app;
+
+/**
+ * @author tomas
+ *
+ */
+public class Hola2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("el tiempo actual en milises: "+ System.currentTimeMillis());
+
+	}
+
+}
